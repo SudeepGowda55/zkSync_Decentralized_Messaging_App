9f2ce076a8ffa25786cc280ac774d2aea38910e4
SudeepGowda55 <ssudeepgowda55@gmail.com>
2023-01-29 15:03:55 +0530

    integration

--- Frontend/pages/_app.tsx
+++ Frontend/pages/_app.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import { zksyncContext } from '../utils/context';
 import { contractDeployedAddress, zkSyncMessagingDAppABI } from '../utils/ContractInfo';
 import { BigNumber } from 'ethers';
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
 
@@ -12,11 +13,12 @@ export default function App({ Component, pageProps }: AppProps) {
   const [accountAddress, setAccountAddress] = useState<string | null>();
   const [signerInstance, setSignerInstance] = useState<Signer | null>();
   const [contractInstance, setContractInstance] = useState<Contract | null>();
+  const [firstName, setFirstName] = useState("");
 
   async function connectionReq() {
     if (window.ethereum) {
       try {
-        const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
+        await window.ethereum.request({ method: "eth_requestAccounts" })
       } catch (error) {
         console.log(error)
       }
@@ -31,15 +33,17 @@ export default function App({ Component, pageProps }: AppProps) {
       try {
         const accounts = await window.ethereum.request({ method: "eth_accounts" })
         if (accounts != null && +window.ethereum.networkVersion == 280) {
-          setAccountAddress(accounts[0]);
           const signer: Signer = (new Web3Provider(window.ethereum)).getSigner();
           setSignerInstance(signer);
           const contract: Contract = new Contract(contractDeployedAddress, zkSyncMessagingDAppABI, signer);
           setContractInstance(contract);
+          console.log("Getting user Info")
+          setFirstName(await contract.getUserName(accounts[0]))
+          setAccountAddress(accounts[0]);
           console.log("Connection Successful");
         }
         else {
-          alert("Please switch network to zkSync!");
+          alert("Please switch Metamask to zkSync! network to continue in this website");
           return;
         }
       } catch (error) {
@@ -51,15 +55,18 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }
 
-  async function convertTimeStamp(time : BigNumber) {
+  async function convertTimeStamp(time: BigNumber) {
     const timeStamp = new Date((time.toNumber()) * 1000);
-    const readableTime = timeStamp.getHours() + "/" + timeStamp.getMinutes() + "/" + timeStamp.getSeconds() + "/" + timeStamp.getDate() + "/" +  timeStamp.getFullYear();
+    const readableTime = timeStamp.getDate() + "/" + timeStamp.getFullYear();
+    // timeStamp.getHours() + "Ho" + timeStamp.getMinutes() + "/" + timeStamp.getSeconds() + "/" + 
     return readableTime;
   }
 
   return (
-    <zksyncContext.Provider value={{ accountAddress, getAccountsInfo, convertTimeStamp, zksyncProvider, signerInstance, contractInstance, connectionReq }}>
-      <Component {...pageProps} />
-    </zksyncContext.Provider>
+    <ChakraProvider>
+      <zksyncContext.Provider value={{ accountAddress, getAccountsInfo, convertTimeStamp, firstName, zksyncProvider, signerInstance, contractInstance, connectionReq }}>
+        <Component {...pageProps} />
+      </zksyncContext.Provider>
+    </ChakraProvider>
   )
 }
